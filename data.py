e07ca98cff34bef0f0709b6a22e62689aec2fd82
YounesNA25 <149947081+YounesNA25@users.noreply.github.com>
2023-11-16 23:26:19 +0100

    changes1

--- data.py
+++ data.py
@@ -3,21 +3,31 @@ from glob import glob
 import torch
 import torch.utils.data
 import torchvision.transforms as transforms
+import pandas as pd
+
+
+
+
 from PIL import Image
 
 class Datasets(torch.utils.data.Dataset):
-    def __init__(self, root, input_size = 178, ext = "jpg"):
-        self.files = glob(os.path.join(root, f"*.{ext}"))
+    def __init__(self, root_images,root_atributes, input_size = 178, ext = "jpg"):
+        self.files = glob(os.path.join(root_images, f"*.{ext}"))
         self.transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize((input_size,input_size))
             ])
+        self.atributes = pd.read_csv("archive/list_attr_celeba.csv", usecols=lambda column: column not in ['image_id'])
+        
 
     def __getitem__(self, index):
         file_path = self.files[index]
         image = Image.open(file_path)
         image = self.transform(image)
-        return image
+        image_attributes = self.atributes.iloc[index].values
+        tensor_attributes = torch.tensor(image_attributes, dtype=torch.float32)
+
+        return image, tensor_attributes
 
     def __len__(self):
         return len(self.files)
