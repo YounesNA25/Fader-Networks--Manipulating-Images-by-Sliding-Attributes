3249ce3a2953431d96a8a8437a72c3c574e936ae
MassylHAMDI <116282135+MassylHAMDI@users.noreply.github.com>
2023-11-16 21:35:57 +0100

    load and test the dataset
    first commit of the file data

---
+++ data.py
@@ -0,0 +1,23 @@
+import os
+from glob import glob
+import torch
+import torch.utils.data
+import torchvision.transforms as transforms
+from PIL import Image
+
+class Datasets(torch.utils.data.Dataset):
+    def __init__(self, root, input_size = 178, ext = "jpg"):
+        self.files = glob(os.path.join(root, f"*.{ext}"))
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((input_size,input_size))
+            ])
+
+    def __getitem__(self, index):
+        file_path = self.files[index]
+        image = Image.open(file_path)
+        image = self.transform(image)
+        return image
+
+    def __len__(self):
+        return len(self.files)
