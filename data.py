3836a03b21dc89bfd2af94c65e8fcf2c7e901c28
MassylHAMDI <116282135+MassylHAMDI@users.noreply.github.com>
2023-11-18 12:22:23 +0100

    update data prces.s

--- data.py
+++ data.py
@@ -11,13 +11,21 @@ import pandas as pd
 from PIL import Image
 
 class Datasets(torch.utils.data.Dataset):
+<<<<<<< Updated upstream
     def __init__(self, root_images,root_atributes, input_size = 178, ext = "jpg"):
+=======
+    def __init__(self, root_images,root_atributes, input_size = 256, ext = "jpg"):
+>>>>>>> Stashed changes
         self.files = glob(os.path.join(root_images, f"*.{ext}"))
         self.transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize((input_size,input_size))
             ])
+<<<<<<< Updated upstream
         self.atributes = pd.read_csv("archive/list_attr_celeba.csv", usecols=lambda column: column not in ['image_id'])
+=======
+        self.atributes = pd.read_csv(root_atributes, usecols=lambda column: column not in ['image_id'])
+>>>>>>> Stashed changes
         
 
     def __getitem__(self, index):
