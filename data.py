48f1fa9f237cf4ba042ce9d844c0e289e2b2cc53
MassylHAMDI <116282135+MassylHAMDI@users.noreply.github.com>
2023-11-18 12:24:28 +0100

    update data prces.s

--- data.py
+++ data.py
@@ -11,21 +11,14 @@ import pandas as pd
 from PIL import Image
 
 class Datasets(torch.utils.data.Dataset):
-<<<<<<< Updated upstream
-    def __init__(self, root_images,root_atributes, input_size = 178, ext = "jpg"):
-=======
     def __init__(self, root_images,root_atributes, input_size = 256, ext = "jpg"):
->>>>>>> Stashed changes
         self.files = glob(os.path.join(root_images, f"*.{ext}"))
         self.transform = transforms.Compose([
             transforms.ToTensor(),
+            transforms.CenterCrop((178, 178)),
             transforms.Resize((input_size,input_size))
             ])
-<<<<<<< Updated upstream
-        self.atributes = pd.read_csv("archive/list_attr_celeba.csv", usecols=lambda column: column not in ['image_id'])
-=======
         self.atributes = pd.read_csv(root_atributes, usecols=lambda column: column not in ['image_id'])
->>>>>>> Stashed changes
         
 
     def __getitem__(self, index):
@@ -33,6 +26,7 @@ class Datasets(torch.utils.data.Dataset):
         image = Image.open(file_path)
         image = self.transform(image)
         image_attributes = self.atributes.iloc[index].values
+        image_attributes = [0 if value == -1 else 1 for value in image_attributes.tolist()]
         tensor_attributes = torch.tensor(image_attributes, dtype=torch.float32)
 
         return image, tensor_attributes
