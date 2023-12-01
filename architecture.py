3918e059693d31211ac9452879546498ba7677b4
YounesNA25 <149947081+YounesNA25@users.noreply.github.com>
2023-12-01 23:18:39 +0100

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -11,7 +11,6 @@ from PIL import Image
 import numpy as np
 
 
-
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
@@ -59,16 +58,11 @@ class Encoder(nn.Module):
         self.encode_layers = [conv1,conv2,conv3,conv4,conv5,conv6,conv7]
 
     def encodage(self, x):
-        """            
-            Arguments:
-                x : Input tensor of size (1, 512, 3, 3)   
-            Returns:
-                enc_output: Output tensor of size (1, 512, 2, 2)
-        """
-        enc_output = x
+        outputs = [x]
         for layer in self.encode_layers:
-            enc_output = layer(enc_output)
-        return enc_output
+            output = layer(outputs[-1])
+            outputs.append(output)
+        return outputs
     
     def forward(self,x):
         enc = self.encodage(x)
@@ -123,53 +117,44 @@ class Decoder(nn.Module):
             nn.Tanh())  # Typically used for normalizing image data between -1 and 1
         
         self.decode_layers = [deconv1,deconv2,deconv3,deconv4,deconv5,deconv6,deconv7]
-    
 
-    """
-    def decodage(self, latent_layer, y):  
-        y = torch.tensor(y)
+    def decodage(self, latent_layers, y):
         y = y.view(1,-1)
         y = y.unsqueeze(2).unsqueeze(3)    
-        batch_size = latent_layer.size(0)  # 1
-        dec_outputs = [latent_layer]
-        
+        batch_size = latent_layers[0].size(0)
+        dec_outputs = [latent_layers[-1]]
         for layer in self.decode_layers:
             output_size = dec_outputs[-1].size(2)
-            input = torch.cat([dec_outputs[-1], y.expand(batch_size, self.n_attributes, output_size, output_size)], dim = 1)
+            input = torch.cat([dec_outputs[-1], y.expand(batch_size, 2*self.n_attributes, output_size, output_size)], dim = 1)
             dec_outputs.append(layer(input))
         return dec_outputs
-    """
-
-    def decodage(self, latent_layer, y):
-            """            
-            Arguments:
-                latent_layer : torch.Tensor - Last layer of the decoder architecture = a tensor of size (1, 512, 2, 2)
-                           y : np.ndarray   - A raw vector of size (1, nb_attributes) or (2, nb_attributes) ?
-                
-            Returns:
-                torch.Tensor: Output tensor of size (1, 3, 256, 256)
-            """
-            
-            y = torch.tensor(y)
-            y = y.view(1, -1)   
-            y = y.unsqueeze(2).unsqueeze(3) 
-
-            batch_size = latent_layer.size(0)  #  1
-            
-            dec_output = [latent_layer]
-            
-            # Iterate through each layer in the decoder
-            for layer in self.decode_layers:              
-                # Concatenate the last output with y along the channel dimension
-                input = torch.cat([dec_output, y.expand(batch_size, self.n_attributes, dec_output.size(2), dec_output.size(2))], dim=1)
-                dec_output = layer(input)
-
-            return dec_output
-
-
-
+    
     def forward(self,enc,y):
         dec = self.decodage(enc,y)
         return dec
 
 
+class Descriminator(nn.Module):
+    
+    def __init__(self,n_attributes):
+        self.n_attributes = n_attributes
+
+        super(Descriminator,self).__init__()
+        self.conv = nn.Sequential( 
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=4, stride=2, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Dropout2d(0.3)
+        )
+
+        self.linear = nn.Sequential(
+            nn.Linear(512, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512,self.n_attributes)
+        )
+
+    def forward(self, latent_representation):
+        x = self.conv(latent_representation)
+        x = x.view(-1, 512)
+        x = self.linear(x)
+        return x
+    
